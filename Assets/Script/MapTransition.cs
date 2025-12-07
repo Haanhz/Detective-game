885b349886a96c6b83e2e7d674c9bc163788d49f
Haanhz <hannahbk476@gmail.com>
2025-12-07 17:09:47 +0700

    map

---
+++ Assets/Script/MapTransition.cs
@@ -0,0 +1,76 @@
+using Unity.Cinemachine;
+using UnityEngine;
+using System.Collections;
+
+
+public class MapTransition : MonoBehaviour
+{
+    public PolygonCollider2D mapBoundary;
+    CinemachineConfiner2D confiner;
+    public Direction direction;
+    public Transform teleportTargetPos;
+    public int additivePos = 4;
+    public enum Direction {Up, Down, Left, Right, Teleport}
+    public CinemachineCamera vcam;
+
+    private void Awake()
+    {
+        confiner = FindFirstObjectByType<CinemachineConfiner2D>();
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // FadeTransition(collision.gameObject);
+            StartCoroutine(FadeTransition(collision.gameObject));
+            // confiner.BoundingShape2D = mapBoundary;
+            // UpdatePlayerPosition(collision.gameObject);
+        }
+    }
+
+    IEnumerator FadeTransition(GameObject player)
+{
+    ScreenFader.Instance.FadeOut();
+
+    // chờ cho fade xong
+    yield return new WaitForSeconds(ScreenFader.Instance.fadeDuration);
+
+    confiner.BoundingShape2D = mapBoundary;
+    UpdatePlayerPosition(player);
+    vcam.PreviousStateIsValid = false;
+    vcam.UpdateCameraState(Vector3.zero, Time.deltaTime); 
+
+    ScreenFader.Instance.FadeIn();
+
+    // chờ fade in kết thúc (nếu cần)
+    yield return new WaitForSeconds(ScreenFader.Instance.fadeDuration);
+}
+    private void UpdatePlayerPosition(GameObject player)
+    {
+        if (direction == Direction.Teleport)
+        {
+            player.transform.position = teleportTargetPos.position;
+            return;
+        }
+        Vector3 newPos = player.transform.position;
+        switch (direction)
+        {
+            case Direction.Up:
+                newPos.y +=additivePos;
+                break;
+            case Direction.Down:
+                newPos.y -=additivePos;
+                break;
+            case Direction.Left:
+                newPos.x -=additivePos;
+                break;
+            case Direction.Right:
+                newPos.x +=additivePos;
+                break;
+            
+        }
+        player.transform.position = newPos;
+    }
+
+}
---
+++ Assets/Script/ScreenFader.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using UnityEngine;
+using Unity.Cinemachine;
+
+public class ScreenFader : MonoBehaviour
+{
+    public static ScreenFader Instance;
+    public CanvasGroup canvasGroup;
+    public float fadeDuration = 1f;
+    public CinemachineCamera vcam;
+
+    CinemachinePositionComposer composer;
+    Vector3 originalDamping;
+
+    Coroutine fadeRoutine;
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else { Destroy(gameObject); return; }
+
+        composer = vcam.GetComponent<CinemachinePositionComposer>();
+
+        if (composer != null)
+            originalDamping = composer.Damping;
+    }
+
+    IEnumerator Fade(float target)
+    {
+        float start = canvasGroup.alpha;
+        float t = 0f;
+
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            canvasGroup.alpha = Mathf.Lerp(start, target, t / fadeDuration);
+            yield return null;
+        }
+
+        canvasGroup.alpha = target;
+    }
+
+    public void FadeOut()
+    {
+        if (fadeRoutine != null) StopCoroutine(fadeRoutine);
+        fadeRoutine = StartCoroutine(FadeRoutine(1, Vector3.zero));
+    }
+
+    public void FadeIn()
+    {
+        if (fadeRoutine != null) StopCoroutine(fadeRoutine);
+        fadeRoutine = StartCoroutine(FadeRoutine(0, originalDamping));
+    }
+
+    IEnumerator FadeRoutine(float alpha, Vector3 damping)
+    {
+        SetDamping(damping);
+        yield return Fade(alpha);
+    }
+
+    void SetDamping(Vector3 d)
+    {
+        if (composer == null) return;
+
+        composer.Damping = d;
+    }
+
+
+}
