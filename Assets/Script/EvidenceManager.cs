59bef328e921c8487a406bd68af9963d841f3d88
Haanhz <hannahbk476@gmail.com>
2025-12-08 17:09:40 +0700

    updaate

---
+++ Assets/Script/EvidenceManager.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvidenceManager : MonoBehaviour
+{
+    public static EvidenceManager Instance;
+
+    public List<string> collectedEvidence = new List<string>();
+    public Dictionary<string, float> evidenceWeights = new Dictionary<string, float>();
+
+    void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+    public void AddEvidence(string tagName, float weight)
+    {
+        collectedEvidence.Add(tagName);
+        evidenceWeights[tagName] = weight;
+
+        Debug.Log($"You found: {tagName} with weight {weight}");
+
+        // In list
+        string listStr = "Collected Evidence: ";
+        foreach (var ev in collectedEvidence)
+            listStr += ev + ", ";
+
+        Debug.Log(listStr);
+
+        // Tính tổng weight
+        float totalWeight = CalculateTotalWeight();
+        Debug.Log("Total Weight = " + totalWeight);
+    }
+
+    public float CalculateTotalWeight()
+    {
+        float total = 0f;
+        foreach (var kv in evidenceWeights)
+            total += kv.Value;
+
+        return total;
+    }
+}
