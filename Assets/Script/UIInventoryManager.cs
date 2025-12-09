725ae63f41722cda793ada872ddd0d23458d92ea
lengochuong134 <165996394+lengochuong134@users.noreply.github.com>
2025-12-09 10:01:48 +0700

    Inventory

---
+++ Assets/Script/UIInventoryManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using TMPro;
+
+public class UIInventoryManager : MonoBehaviour
+{
+    public static UIInventoryManager Instance;
+
+    [Header("UI References")]
+    public GameObject inventoryPanel;
+    public Transform content;
+    public GameObject entryPrefab;
+
+    private bool isOpen = false;
+
+    void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        // Toggle inventory bằng I hoặc Tab
+        if (Input.GetKeyDown(KeyCode.I) || Input.GetKeyDown(KeyCode.Tab))
+        {
+            isOpen = !isOpen;
+            inventoryPanel.SetActive(isOpen);
+
+            if (isOpen) RefreshUI();
+        }
+    }
+
+    public void RefreshUI()
+    {
+        // Xóa tất cả dòng cũ
+        foreach (Transform child in content)
+            Destroy(child.gameObject);
+
+        // Tạo lại từng dòng
+        foreach (string evidenceName in EvidenceManager.Instance.collectedEvidence)
+        {
+            GameObject entry = Instantiate(entryPrefab, content);
+            entry.GetComponent<TextMeshProUGUI>().text = evidenceName;
+        }
+    }
+}
