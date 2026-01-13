3cc5ff7315020a30809856dc25f274288ef6b715
Haanhz <hannahbk476@gmail.com>
2026-01-13 14:22:27 +0700

    FIX

--- Assets/Script/Note.cs
+++ Assets/Script/Note.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Note : MonoBehaviour
 {
@@ -8,35 +9,67 @@ public class Note : MonoBehaviour
 
     private Transform player;
     private bool isNoteOpen = false;
+    public Button closeBtn;
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        
+        // Đăng ký sự kiện click cho close button
+        if (closeBtn != null)
+        {
+            closeBtn.onClick.AddListener(CloseNoteButton);
+        }
     }
 
     void Update()
-{
-    float dist = Vector2.Distance(player.position, transform.position);
-
-    if (dist <= interactRange && Input.GetKeyDown(KeyCode.F))
     {
-        if (!isNoteOpen)
+        float dist = Vector2.Distance(player.position, transform.position);
+
+        if (dist <= interactRange && Input.GetKeyDown(KeyCode.F))
         {
-            UIManager.Instance.OpenNote(content);
-            isNoteOpen = true;
+            if (!isNoteOpen)
+            {
+                UIManager.Instance.OpenNote(content);
+                isNoteOpen = true;
+            }
+            else
+            {
+                UIManager.Instance.CloseNote();
+                isNoteOpen = false;
+            }
         }
-        else
+        
+        // Có thể thêm phím ESC để đóng note
+        if (isNoteOpen && Input.GetKeyDown(KeyCode.Escape))
         {
             UIManager.Instance.CloseNote();
             isNoteOpen = false;
         }
     }
-    
-}
+
+    // Hàm xử lý khi click close button
+    void CloseNoteButton()
+    {
+        if (isNoteOpen)
+        {
+            UIManager.Instance.CloseNote();
+            isNoteOpen = false;
+        }
+    }
 
     void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, interactRange);
+    }
+    
+    // Cleanup khi destroy object
+    void OnDestroy()
+    {
+        if (closeBtn != null)
+        {
+            closeBtn.onClick.RemoveListener(CloseNoteButton);
+        }
     }
 }
